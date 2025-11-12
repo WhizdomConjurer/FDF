40326b2f8e39be5c818f8abc2ddf6065cbb1b3b5
Whiz <riebschlaeger1998@gmail.com>
2025-11-12 17:30:09 +0100

    nothing to say

--- includes/libft/lst/ft_lstiter.c
+++ includes/libft/lst/ft_lstiter.c
@@ -6,7 +6,7 @@
 /*   By: puzzlesanalytik <puzzlesanalytik@studen    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/12 17:25:41 by puzzlesanal       #+#    #+#             */
-/*   Updated: 2025/11/12 17:26:17 by puzzlesanal      ###   ########.fr       */
+/*   Updated: 2025/11/12 17:26:45 by puzzlesanal      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
